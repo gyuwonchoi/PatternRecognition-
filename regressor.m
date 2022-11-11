ab363194a12c48759106d7db4ea31fc4279ba834
Gyuwon Choi <kafoot@naver.com>
2022-11-11 19:30:01 +0900

    Updated considering batch size

--- regressor.m
+++ regressor.m
@@ -5,63 +5,68 @@ trainset = load_data('./regressor_trn.txt');
 testset = load_data('./regressor_tst.txt');
 
 % data param
-data_num = length(trainset); % sample num 
+data_num = length(trainset);  % sample num 
 
 % train param 
 node_num = 10;                % num of nodes in hidden layer 
-input_num = 2+1;             % x1, x2, and bias -1
+input_num = 2+1;              % x1, x2, and bias -1
+batch_num = 225;
 epoch = 50000;
-lr = 0.0000001;                   % learning rate?
+lr = 0.001;                   % learning rate
 
 % input initialization 
-input = trainset(:, 1:2);
-bias  = -ones(data_num, 1);
-input = [input  bias];
+input = trainset(:, 1:2, :);
+input(:, 3, :)= -1;           % 225 x 3 
 
-dk = trainset(:, 3);
+dk = trainset(:, 3, :);       % groudn truth 
 
-% weights declariation 
-wj = normrnd(0, 1, [input_num, node_num])    % node_num x 3
-wk = normrnd(0, 1, [node_num, 1]);           % 3x1
+% weights declariation : row x column x batch   
+wj = normrnd(0, 1, [input_num, node_num, batch_num]);    
+wk = normrnd(0, 1, [node_num, 1, batch_num]);        
 
-x = 0
-y = -inf
+x = 0;
+y = -inf;
+
 % train : Neuron j & k 
 for i= 1:epoch
     % forward
-    vj = input * wj;           % neuron j
+    vj = pagemtimes(input , wj);           % 225 x sample_num x node_num 
     yj = sigmoid(vj);
 
-    vk = yj * wk;              % neuron k
+    vk = pagemtimes(yj , wk);              
     yk = sigmoid(vk);
 
     error = dk - yk;         
-    error_batch = diag(error * error.');
-    error_sum = sum(error_batch, 1);
+    error_batch = pagemtimes(error, error);
+    error_sum = sum(error_batch, 3);
 
-    % backward 
-    delta_k = error * yk.'* (1-yk);     % neuron k
-    wght_del_k = lr * delta_k.' * yj;   % batch mode 
-    size(yj)
+    % neuron k
+    delta_k = pagemtimes(error, pagemtimes(yk, 1-yk));    
+    wght_del_k = lr * pagemtimes(delta_k, yj);  
 
-    % delta_j_sig = sum(delta_k * wk.',2); % each neuron(6) in each sample
-    delta_j_sig = delta_k * wk.'; % 225 x 6 
-    delta_j = diag(yj* (1-yj.'))+ delta_j_sig; % 225 x 6
-
-    % wght_del_j = lr * delta_j.' * input;        
-    wght_del_j = lr * input.' * delta_j; % 3x6
+    % neuron j
+    delta_j_sig = pagemtimes(wk, delta_k); 
+    delta_j = diagonal(pagemtimes(yj, 'transpose', (1-yj), 'none'));
+    delta_j = delta_j + delta_j_sig;
+       
+    wght_del_j = lr * pagemtimes(delta_j, input); 
 
     % update weight 
-    wj = wj + wght_del_j;
-    wk = wk + wght_del_k.';
-    
-    size(wght_del_k)
+    wj = wj + pagetranspose(wght_del_j);
+    wk = wk + pagetranspose(wght_del_k);
+
     % draw chart error_sum x epoch
     y = [y error_sum];
     x = [x i];
 end
 
 plot(x,y)
+
+function out = diagonal(input)    
+    for i=1:225
+        out(:,:,i)=  diag(input(:,:,i));
+    end
+end
 
 function data = sigmoid_prime(output)
     data = output.* (1 -output);
@@ -73,12 +78,11 @@ function mat = load_data(path)
 
     data = textscan(fid, '%f%f%f', 'Delimiter','\t');
     
-    mat= zeros(225,3);
-    mat(:, 1)= data{1,1};
-    mat(:, 2)= data{1,2};
-    mat(:, 3)= data{1,3};
+    mat= zeros(1, 3, 225);
+    mat(1, 1, :)= data{1,1};
+    mat(1, 2, :)= data{1,2};
+    mat(1, 3, :)= data{1,3};
     
     fclose(fid);
 end
-
 
